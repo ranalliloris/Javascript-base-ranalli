5c8e8dd11df1ba2c3b9f603b065ea59c3dbfd1fe
lorisranalli <lorisranalli@isisdavinci.eu>
2023-03-09 14:38:32 +0100

    Esercizi Lezione 4 del 08-03-2023

---
+++ listaSpesa/js/listaSpesa.js
@@ -0,0 +1,13 @@
+
+let ulLista=document.getElementById("lista");
+
+console.log(ulLista);
+
+let liLista=document.getElementsByTagName("li");
+console.log(liLista);
+
+let liAlimento=document.getElementsByClassName("alimento");
+console.log(liAlimento);
+
+let liAlimento2=document.querySelectorAll(".alimento");
+console.log(liAlimento2);
